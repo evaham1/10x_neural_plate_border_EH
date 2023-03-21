b9f0c4b52dd02cb702bd2d1860f89fa1cfed0f60
Eva <ehamrud@gmail.com>
2023-03-21 10:04:52 +0000

    add nxf values

--- run_scripts_eva/NF-downstream_analysis_crick_stacas.sh
+++ run_scripts_eva/NF-downstream_analysis_crick_stacas.sh
@@ -5,14 +5,16 @@
 #SBATCH --mail-user=eva.hamrud@crick.ac.uk
 
 export TERM=xterm
+export NXF_VER=20.07.1
+export NXF_SINGULARITY_CACHEDIR=/nemo/lab/briscoej/working/hamrude/NF_singularity
+export NXF_HOME=/flask/scratch/briscoej/hamrude/10x_neural_plate_border_EH/NF-downstream_analysis
+export NXF_WORK=work/
 
 ## LOAD REQUIRED MODULES
 ml purge
 ml Nextflow/20.07.1
 ml Singularity/3.4.2
 ml Graphviz
-
-export NXF_VER=20.07.1
 
 nextflow run ./NF-downstream_analysis/main.nf \
 --input ./NF-downstream_analysis/samplesheet.csv \
